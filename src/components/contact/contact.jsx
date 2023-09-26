b952a4996bf82f04d2f93d3a7de29d2c6d2c3fe0
Jacob Prince <jacobpchandy2003@gmail.com>
2023-09-27 00:24:25 +0530

    final

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -27,6 +27,8 @@ const Contact = () => {
       .then(
         (result) => {
           console.log(result.text);
+          e.target.reset();
+          alert("Email Sent !");
         },
         (error) => {
           console.log(error.text);
